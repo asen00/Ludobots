ebb7ca30891ff82107c1aadcee2348d0bb823c00
Antara Sen <antarasen2027@u.northwestern.edu>
2023-01-06 15:47:42 -0600

    Create test1.py

---
+++ test1.py
@@ -0,0 +1,12 @@
+import pybullet as p
+import time
+
+physicsClient = p.connect(p.GUI)
+p.loadSDF("box.sdf")
+for i in range(1000):
+    p.stepSimulation()
+    print(i)
+    t=1/60
+    time.sleep(t)
+
+p.disconnect(p.GUI)
