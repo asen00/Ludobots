74abd3cc88af92cfc9ef9cd8be6bd1c34f6ae1da
Antara Sen <antarasen2027@u.northwestern.edu>
2023-01-09 21:41:36 -0600

    manylinks task

--- generate.py
+++ generate.py
@@ -1,5 +1,13 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("box.sdf")
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
+pyrosim.Start_SDF("boxes.sdf")
+#pyrosim.Send_Cube(name="Box1", pos=[0,0,0] , size=[1,1,1])
+#pyrosim.Send_Cube(name="Box2", pos=[0,0,0] , size=[1,1,1])
+
+for i in range(5):
+    for j in range(5):
+        for k in range(10):
+            sz = 1*(1-(k/10))
+            pyrosim.Send_Cube(name="Box", pos=[i,j,sz] , size=[1-sz,1-sz,1-sz])
+
 pyrosim.End()
