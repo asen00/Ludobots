4d2cb3fd458533edd03d09a4edd3d7fd8f561942
Antara Sen <antarasen2027@u.northwestern.edu>
2023-01-09 21:32:15 -0600

    Task D

--- generate.py
+++ generate.py
@@ -1,5 +1,12 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("box.sdf")
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
+pyrosim.Start_SDF("boxes.sdf")
+#pyrosim.Send_Cube(name="Box1", pos=[0,0,0] , size=[1,1,1])
+#pyrosim.Send_Cube(name="Box2", pos=[0,0,0] , size=[1,1,1])
+for j in range(5):
+    for k in range(5):
+        for i in range(10):
+            side = 1*(1-(i/10))
+            pyrosim.Send_Cube(name="Box1", pos=[j,k,side] , size=[1-side,1-side,1-side])
+
 pyrosim.End()
