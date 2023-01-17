57b1a1ca9ea340e219ad8dc47bdcb9762532c36e
Antara Sen <antarasen2027@u.northwestern.edu>
2023-01-17 03:50:31 -0600

    Update analyze.py

--- analyze.py
+++ analyze.py
@@ -1,6 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+targetAngles = np.load("data/sinplotTest.npy")
+plt.plot(np.linspace(0, 2*np.pi, 10000), targetAngles)
+plt.show()
+exit()
+
 backLegSensorValues = np.load("data/backsensorVals.npy")
 frontLegSensorValues = np.load("data/frontsensorVals.npy")
 plt.plot(backLegSensorValues, label="Back Leg", linestyle="--")
