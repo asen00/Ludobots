513828a8adafac3b9f298f9c9da6bed1a94e93c6
Antara Sen <antarasen2027@u.northwestern.edu>
2023-01-16 20:49:17 -0600

    plot modification

--- analyze.py
+++ analyze.py
@@ -3,7 +3,9 @@ import matplotlib.pyplot as plt
 
 backLegSensorValues = np.load("data/backsensorVals.npy")
 frontLegSensorValues = np.load("data/frontsensorVals.npy")
-plt.plot(backLegSensorValues, label="Back Leg")
-plt.plot(frontLegSensorValues, label="Front Leg")
+plt.plot(backLegSensorValues, label="Back Leg", linestyle="--")
+plt.plot(frontLegSensorValues, label="Front Leg", linestyle="--")
 plt.legend()
+plt.title("Sensor Values")
+plt.savefig("sensorvals.png", dpi=300)
 plt.show()
