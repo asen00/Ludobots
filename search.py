39929918aa3b8ce4e0e31a8f04c53812bf003add
Antara Sen <antarasen2027@u.northwestern.edu>
2023-01-29 16:19:30 -0600

    Until step 22

--- search.py
+++ search.py
@@ -1,5 +1,11 @@
 import os
 
-for i in range(5):
+from hillclimber import HILL_CLIMBER
+
+hc = HILL_CLIMBER()
+
+hc.Evolve()
+
+'''for i in range(5):
     os.system("/Users/AntaraSen_1/opt/anaconda3/bin/python generate.py")
-    os.system("/Users/AntaraSen_1/opt/anaconda3/bin/python test1.py")
+    os.system("/Users/AntaraSen_1/opt/anaconda3/bin/python test1.py")'''
---
+++ solution.py
@@ -0,0 +1,41 @@
+import pyrosim.pyrosim as pyrosim
+import numpy as np
+
+class SOLUTION:
+    def __init__(self):        
+        self.weights = (2*np.random.rand(3,2))-1
+    
+    def Evaluate(self):
+        pass
+
+    def Create_World():
+        pyrosim.Start_SDF("world.sdf")
+        pyrosim.Send_Cube(name="Box", pos=[0,3,0.5] , size=[1,1,1])
+        pyrosim.End()
+
+    def Generate_Body():
+        pyrosim.Start_URDF("body.urdf")
+        pyrosim.Send_Cube(name="Torso", pos=[0,0,1.5] , size=[1,1,1])
+        pyrosim.Send_Joint( name = "Torso_FrontLeg" , parent= "Torso" , child = "FrontLeg" , type = "revolute", position = [0.5,0,1])
+        pyrosim.Send_Cube(name="FrontLeg", pos=[0.5,0,-0.5] , size=[1,1,1])
+        pyrosim.Send_Joint( name = "Torso_BackLeg" , parent= "Torso" , child = "BackLeg" , type = "revolute", position = [-0.5,0,1])
+        pyrosim.Send_Cube(name="BackLeg", pos=[-0.5,0,-0.5] , size=[1,1,1])
+        pyrosim.End()
+
+    def Generate_Brain():
+        pyrosim.Start_NeuralNetwork("brain.nndf")
+        
+        pyrosim.Send_Sensor_Neuron(name = 0 , linkName = "Torso")
+        pyrosim.Send_Sensor_Neuron(name = 1 , linkName = "BackLeg")
+        pyrosim.Send_Sensor_Neuron(name = 2 , linkName = "FrontLeg")
+
+        pyrosim.Send_Motor_Neuron( name = 3 , jointName = "Torso_BackLeg")
+        pyrosim.Send_Motor_Neuron( name = 4 , jointName = "Torso_FrontLeg")
+
+        pyrosim.Send_Synapse(sourceNeuronName = 0 , targetNeuronName = 3 , weight = 1.0)
+        pyrosim.Send_Synapse(sourceNeuronName = 1 , targetNeuronName = 3 , weight = 1.0)
+
+        pyrosim.Send_Synapse(sourceNeuronName = 1 , targetNeuronName = 4 , weight = 0.5)
+        pyrosim.Send_Synapse(sourceNeuronName = 2 , targetNeuronName = 4 , weight = 0.5)
+        
+        pyrosim.End()
