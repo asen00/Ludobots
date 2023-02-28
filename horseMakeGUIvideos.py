f39a89dbd8a3e0138d59406687bbf33363dcb3e5
Antara Sen <antarasen2027@u.northwestern.edu>
2023-02-27 23:36:47 -0600

    Commit

--- horseMakeGUIvideos.py
+++ horseMakeGUIvideos.py
@@ -4,7 +4,7 @@ import time
 import constants as c
 
 timestamp = '02-27-23_22:45:34'
-run = 1
+run = 1 
 IDsForGUIvideos = np.load('IDforGUIvidoes_'+timestamp+'.npy')
 print(IDsForGUIvideos)
 os.system("/Users/AntaraSen_1/opt/anaconda3/bin/python horseSimulate.py " + " GUI " + str(IDsForGUIvideos[0][run]) + " 2>&1 &")
